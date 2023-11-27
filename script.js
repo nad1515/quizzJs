00857ce7c10b3e7bd18897b7e4b1fddeb6f0ea62
nadia <anabelle0025@yahoo.fr>
2023-11-27 16:03:23 +0100

    a finir

--- script.js
+++ script.js
@@ -54,10 +54,53 @@ const questions = [
 /*************************/
 /* Contenu du DOM chargé */
 /*************************/
+
 document.addEventListener("DOMContentLoaded", () => {
+let questionIndex = 0;
+let score;
+let affichAnswers = document.getElementById("answers");
+let affichScore = document.getElementById("score");
+   
+let affichQuestion= document.getElementById("question");
+  affichQuestion.textContent = questions[questionIndex].question;
+  console.log(questions[questionIndex]);
+genereQuestion();
+function genereQuestion() {
+  questions[questionIndex].answers.forEach(answer => {
+    console.log(answer)
+    affichQuestion.textContent = questions[questionIndex].question;
+      let li = document.createElement("li");
+      li.classList.add("answer");
+       li.textContent = answer;
   
-// démarrage du quizz
+       affichAnswers.appendChild(li);
+      
+      
+   });
+}
+
+affichAnswers.addEventListener("click",(event)=>{
+ console.log(event.target.innerText);
+ 
+ affichAnswers.innerHTML = "";
+  if(event.target.innerText === questions[questionIndex].correctAnswerIndex){
+
+   score ++;
+   affichScore.textContent = score;
+
+ }
+ questionIndex ++;
+   if(questionIndex < questions.length ){ 
+     genereQuestion();
+
+   
+   } else{
+    affichQuestion.textContent = "Merci d'avoir repondu";
+    affichAnswers.innerHTML = "";
+
+   }
+  
+ });
 
 
-  
-});
+ });
